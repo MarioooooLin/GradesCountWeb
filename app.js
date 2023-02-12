7ddc187f8e0b56e74f2ac1dbaf13be61f26470df
Mariooooo <lintt74123@gmail.com>
2023-02-13 01:09:24 +0800

    'Add' and 'Trash' button complete

--- app.js
+++ app.js
@@ -29,3 +29,244 @@ allButtons.forEach((button) => {
         e.preventDefault();
     });
 });
+
+//選擇select標籤後改變顏色
+let allSelects = document.querySelectorAll("select");
+allSelects.forEach((select) => {
+    select.addEventListener("change", (e) => {
+        setGPA();
+        changeColor(e.target);
+    });
+});
+//改變credit後更新GPA
+let credits = document.querySelectorAll(".class-credit");
+credits.forEach((credit) => {
+    credit.addEventListener("change", () => {
+        setGPA();
+    });
+});
+
+function changeColor(target) {
+    if (target.value == "A" || target.value == "A-") {
+        target.style.backgroundColor = "red";
+        target.style.color = "black";
+    } else if (target.value == "B+" || target.value == "B" || target.value == "B-") {
+        target.style.backgroundColor = "Yellow";
+        target.style.color = "black";
+    } else if (target.value == "C+" || target.value == "C" || target.value == "C-") {
+        target.style.backgroundColor = "Green";
+        target.style.color = "black";
+    } else if (target.value == "D+" || target.value == "D" || target.value == "D-") {
+        target.style.backgroundColor = "purple";
+        target.style.color = "black";
+    } else if (target.value == "F") {
+        target.style.backgroundColor = "orange";
+        target.style.color = "black";
+    } else {
+        target.style.backgroundColor = "gray";
+        target.style.color = "white";
+    }
+}
+//級別分數轉換
+function convertor(grade) {
+    switch (grade) {
+        case "A":
+            return 4.0;
+        case "A-":
+            return 3.7;
+        case "B+":
+            return 3.4;
+        case "B":
+            return 3.0;
+        case "B-":
+            return 2.7;
+        case "C+":
+            return 2.4;
+        case "C":
+            return 2.0;
+        case "C-":
+            return 1.7;
+        case "D+":
+            return 1.4;
+        case "D":
+            return 1.0;
+        case "D-":
+            return 0.7;
+        case "F":
+            return 0.0;
+        default:
+            return 0;
+    }
+}
+
+//計算GPA
+function setGPA() {
+    let formLength = document.querySelectorAll("form").length;
+    let credits = document.querySelectorAll(".class-credit");
+    let selects = document.querySelectorAll("select");
+    let sum = 0; //計算用分子
+    let creditSum = 0; //計算用分母
+
+    for (let i = 0; i < credits.length; i++) {
+        if (!isNaN(credits[i].valueAsNumber)) {
+            creditSum += credits[i].valueAsNumber;
+        }
+    }
+    console.log(creditSum); //Test
+    for (let i = 0; i < formLength; i++) {
+        if (!isNaN(credits[i].valueAsNumber)) {
+            sum += credits[i].valueAsNumber * convertor(selects[i].value);
+        }
+        let result;
+        if (creditSum == 0) {
+            result = (0.0).toFixed(2);
+        } else {
+            result = (sum / creditSum).toFixed(2);
+        }
+        document.getElementById("result-gpa").innerText = result;
+    }
+}
+
+//點按後增加格子
+let addButton = document.querySelector(".plus-btn");
+addButton.addEventListener("click", () => {
+    let newForm = document.createElement("form");
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("grader");
+
+    //製作5個元素
+    let newInput1 = document.createElement("input");
+    newInput1.setAttribute("type", "text");
+    newInput1.setAttribute("list", "opt");
+    newInput1.classList.add("class-type");
+
+    let newInput2 = document.createElement("input");
+    newInput2.setAttribute("type", "text");
+    newInput2.classList.add("class-number");
+
+    let newInput3 = document.createElement("input");
+    newInput3.setAttribute("type", "number");
+    newInput3.setAttribute("min", "0");
+    newInput3.setAttribute("max", "6");
+    newInput3.classList.add("class-credit");
+    newInput3.addEventListener("change", () => {
+        setGPA();
+    });
+
+    // select選單
+    let newSelect = document.createElement("select");
+    newSelect.classList.add("select");
+    var opt1 = document.createElement("option");
+    opt1.setAttribute("value", "");
+    let textNode1 = document.createTextNode("");
+    opt1.appendChild(textNode1);
+    var opt2 = document.createElement("option");
+    opt2.setAttribute("value", "A");
+    let textNode2 = document.createTextNode("A");
+    opt2.appendChild(textNode2);
+    var opt3 = document.createElement("option");
+    opt3.setAttribute("value", "A-");
+    let textNode3 = document.createTextNode("A-");
+    opt3.appendChild(textNode3);
+    var opt4 = document.createElement("option");
+    opt4.setAttribute("value", "B+");
+    let textNode4 = document.createTextNode("B+");
+    opt4.appendChild(textNode4);
+    var opt5 = document.createElement("option");
+    opt5.setAttribute("value", "B");
+    let textNode5 = document.createTextNode("B");
+    opt5.appendChild(textNode5);
+    var opt6 = document.createElement("option");
+    opt6.setAttribute("value", "B-");
+    let textNode6 = document.createTextNode("B-");
+    opt6.appendChild(textNode6);
+    var opt7 = document.createElement("option");
+    opt7.setAttribute("value", "C+");
+    let textNode7 = document.createTextNode("C+");
+    opt7.appendChild(textNode7);
+    var opt8 = document.createElement("option");
+    opt8.setAttribute("value", "C");
+    let textNode8 = document.createTextNode("C");
+    opt8.appendChild(textNode8);
+    var opt9 = document.createElement("option");
+    opt9.setAttribute("value", "C-");
+    let textNode9 = document.createTextNode("C-");
+    opt9.appendChild(textNode9);
+    var opt10 = document.createElement("option");
+    opt10.setAttribute("value", "D+");
+    let textNode10 = document.createTextNode("D+");
+    opt10.appendChild(textNode10);
+    var opt11 = document.createElement("option");
+    opt11.setAttribute("value", "D");
+    let textNode11 = document.createTextNode("D");
+    opt11.appendChild(textNode11);
+    var opt12 = document.createElement("option");
+    opt12.setAttribute("value", "D-");
+    let textNode12 = document.createTextNode("D-");
+    opt12.appendChild(textNode12);
+    var opt13 = document.createElement("option");
+    opt13.setAttribute("value", "F");
+    let textNode13 = document.createTextNode("F");
+    opt13.appendChild(textNode13);
+
+    newSelect.appendChild(opt1);
+    newSelect.appendChild(opt2);
+    newSelect.appendChild(opt3);
+    newSelect.appendChild(opt4);
+    newSelect.appendChild(opt5);
+    newSelect.appendChild(opt6);
+    newSelect.appendChild(opt7);
+    newSelect.appendChild(opt8);
+    newSelect.appendChild(opt9);
+    newSelect.appendChild(opt10);
+    newSelect.appendChild(opt11);
+    newSelect.appendChild(opt12);
+    newSelect.appendChild(opt13);
+
+    newSelect.addEventListener("change", (e) => {
+        setGPA();
+        changeColor(e.target);
+    });
+
+    let newButton = document.createElement("button");
+    newButton.classList.add("trash-button");
+    let newItag = document.createElement("i");
+    newItag.classList.add("fas");
+    newItag.classList.add("fa-trash");
+    newButton.appendChild(newItag);
+
+    newButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.target.parentElement.parentElement.style.animation = "scaleDown 0.5s ease forwards";
+        e.target.parentElement.parentElement.addEventListener("animationend", (e) => {
+            e.target.remove();
+            setGPA();
+        });
+    });
+
+    newDiv.appendChild(newInput1);
+    newDiv.appendChild(newInput2);
+    newDiv.appendChild(newInput3);
+    newDiv.appendChild(newSelect);
+    newDiv.appendChild(newButton);
+
+    newForm.appendChild(newDiv);
+    document.querySelector(".all-inputs").appendChild(newForm);
+    newForm.style.animation = "scaleUp 0.5s ease forwards";
+});
+
+//垃圾桶
+let allTrash = document.querySelectorAll(".trash-button");
+allTrash.forEach((trash) => {
+    trash.addEventListener("click", (e) => {
+        e.target.parentElement.parentElement.classlist.add("remove");
+        //e.target.parentElement.parentElement.remove()
+    });
+});
+allTrash.forEach((trash) => {
+    let form = trash.parentElement.parentElement;
+    form.addEventListener("transitionend", (e) => {
+        e.target.remove();
+        setGPA();
+    });
+});
